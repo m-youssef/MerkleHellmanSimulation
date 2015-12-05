7de58a1904a10238c7aa4a768abb0bd332d774f6
Mahmoud Youssef <eng_mahmoud9012@yahoo.com>
2015-12-06 00:56:39 +0200

    Encrypting a Message using Merkle-Hellman Knapsacks

---
+++ MerkleHellmanSimulation/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MerkleHellmanSimulation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var s = textBox1.Text.TrimEnd(',').Split(',').ToList().Select(z=>Convert.ToInt32(z)).ToList();
+            var w = Convert.ToInt32(textBox2.Text);
+            var n = Convert.ToInt32(textBox3.Text);
+
+            var h = MyMerkleHellman.CalculateHardknapsacks(s, w, n);
+
+            textBox5.Text = string.Join(",", h);
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            textBox4.Text= textBox1.Text.TrimEnd(',').Split(',').ToList().Select(z => Convert.ToInt32(z)).ToList().Count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var h = textBox5.Text.TrimEnd(',').Split(',').ToList().Select(z => Convert.ToInt32(z)).ToList();
+            var enc = MyMerkleHellman.EncruptMessage(h, textBox6.Text);
+            textBox7.Text = string.Join(",", enc);
+        }
+    }
+}
---
+++ MerkleHellmanSimulation/MyMerkleHellman.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MerkleHellmanSimulation
+{
+    public class MyMerkleHellman
+    {
+
+        public static List<int> CalculateHardknapsacks(List<int> simlpeknapsacks, int w, int n)
+        {
+            return simlpeknapsacks.Select(h => (h*w) % n).ToList();
+        }
+
+        public static List<int> EncruptMessage(List<int> hard, string message)
+        {
+            var m = hard.Count;
+            var list = Split(message.ToList(), m);
+            var result=new List<int>();
+            foreach (var ele in list)
+            {
+                result.Add(hard.Select((t, i) => ele[i]*t).Sum());
+            }
+            return result;
+            
+        }
+
+        public static List<List<int>> Split(List<char> source,int sub)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / sub)
+                .Select(x => x.Select(v => Convert.ToInt32(v.Value.ToString())).ToList())
+                .ToList();
+        }
+
+    }
+}

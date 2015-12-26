5a50d284acfc54a66cd88de225c6fe3011163fee
Mahmoud Youssef <eng_mahmoud9012@yahoo.com>
2015-12-26 03:24:26 +0200

    add function to encrypt / decrypt message

--- MerkleHellmanSimulation/Form1.cs
+++ MerkleHellmanSimulation/Form1.cs
@@ -12,37 +12,73 @@ namespace MerkleHellmanSimulation
 {
     public partial class Form1 : Form
     {
+        public delegate void UpdateTextBoxDelegate(String textBoxString); // delegate type 
+        public UpdateTextBoxDelegate UpdateTextBox; // delegate object
+        public MyMerkleHellman MyMerkleHellmanClass;
+        void UpdateTextBox1(string str) { textBox9.AppendText(str + Environment.NewLine); } // this method is invoked
+
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
+            UpdateTextBox = new UpdateTextBoxDelegate(UpdateTextBox1); // initialize delegate object
+            MyMerkleHellmanClass = new MyMerkleHellman(this);
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var s = textBox1.Text.TrimEnd(',').Split(',').ToList().Select(z=>Convert.ToInt32(z)).ToList();
+
+            var ee = textBox1.Text[0];
+            var s = textBox1.Text.TrimEnd(',').Split(',').ToList().Select(z => Convert.ToInt32(z)).ToList();
             var w = Convert.ToInt32(textBox2.Text);
             var n = Convert.ToInt32(textBox3.Text);
 
-            var h = MyMerkleHellman.CalculateHardknapsacks(s, w, n);
+            var h = MyMerkleHellmanClass.CalculateHardknapsacks(s, w, n);
 
             textBox5.Text = string.Join(",", h);
         }
 
         private void textBox1_Leave(object sender, EventArgs e)
         {
-            textBox4.Text= textBox1.Text.TrimEnd(',').Split(',').ToList().Select(z => Convert.ToInt32(z)).ToList().Count.ToString();
+            textBox4.Text = textBox1.Text.TrimEnd(',').Split(',').ToList().Select(z => Convert.ToInt32(z)).ToList().Count.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             var h = textBox5.Text.TrimEnd(',').Split(',').ToList().Select(z => Convert.ToInt32(z)).ToList();
-            var enc = MyMerkleHellman.EncruptMessage(h, textBox6.Text);
+            var enc = MyMerkleHellmanClass.EncryptMessage(h, textBox6.Text.Replace(" ", ""));
             textBox7.Text = string.Join(",", enc);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var msg = textBox7.Text.TrimEnd(',').Split(',').ToList().Select(z => Convert.ToInt32(z)).ToList();
+            var w = Convert.ToInt32(textBox2.Text);
+            var n = Convert.ToInt32(textBox3.Text);
+            var wi = MyMerkleHellmanClass.CalculateWinvers(w, n);
+            var s = textBox1.Text.TrimEnd(',').Split(',').ToList().Select(z => Convert.ToInt32(z)).ToList();
+
+            var messge = MyMerkleHellmanClass.DecryptMessage(msg, wi, n, s);
+            string str = "";
+            foreach (var list in messge)
+            {
+                str += string.Join("", list);
+
+            }
+            textBox6.Text = str;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var w = Convert.ToInt32(textBox2.Text);
+            var n = Convert.ToInt32(textBox3.Text);
+            var wi = MyMerkleHellmanClass.CalculateWinvers(w, n);
+            textBox8.Text = wi.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox9.Clear();
         }
     }
 }
--- MerkleHellmanSimulation/MyMerkleHellman.cs
+++ MerkleHellmanSimulation/MyMerkleHellman.cs
@@ -6,26 +6,87 @@ namespace MerkleHellmanSimulation
 {
     public class MyMerkleHellman
     {
+        private readonly Form1 _myFormControl;
 
-        public static List<int> CalculateHardknapsacks(List<int> simlpeknapsacks, int w, int n)
+        public MyMerkleHellman(Form1 myForm)
         {
-            return simlpeknapsacks.Select(h => (h*w) % n).ToList();
+            _myFormControl = myForm;
+
         }
 
-        public static List<int> EncruptMessage(List<int> hard, string message)
+        public List<int> CalculateHardknapsacks(List<int> simlpeknapsacks, int w, int n)
+        {
+            PrintToScreen("**** Calculating Hard knapsacks ****");
+            var res=new List<int>();
+            for (int i = 0; i < simlpeknapsacks.Count; i++)
+            {
+                var h = (simlpeknapsacks[i]*w)%n;
+                PrintToScreen(simlpeknapsacks[i] + " * " + w + " mod " + n + " = " + h);
+                res.Add(h);
+            }
+            PrintToScreen("H = ["+string.Join(",", res)+"]");
+            PrintToScreen("**************************************");
+            return res;
+           // return simlpeknapsacks.Select(h => (h * w) % n).ToList();
+        }
+
+        public List<int> EncryptMessage(List<int> hard, string message)
         {
             var m = hard.Count;
             var list = Split(message.ToList(), m);
-            var result=new List<int>();
+            var list1 = CheckMessageLength(list, hard.Count);
+            var result = new List<int>();
             foreach (var ele in list)
             {
-                result.Add(hard.Select((t, i) => ele[i]*t).Sum());
+                result.Add(hard.Select((t, i) => ele[i] * t).Sum());
             }
             return result;
-            
+
         }
 
-        public static List<List<int>> Split(List<char> source,int sub)
+        // W-1 = (w^n-2)%n
+        public int CalculateWinvers(int w, int n)
+        {
+            PrintToScreen("**** Calculating W Inverse ****");
+            PrintToScreen("**** W-1 = (w^n-2)%n ****");
+            var wi= (int)(Math.Pow(w, n - 2) % n);
+            PrintToScreen(" W-1 = " + wi);
+            PrintToScreen("**************************************");
+            return wi;
+        }
+
+        private void PrintToScreen(string text)
+        {
+            _myFormControl.Invoke(_myFormControl.UpdateTextBox, text); // updates textbox1 on winForm
+
+        }
+
+        private List<List<int>> CheckMessageLength(List<List<int>> messge, int size)
+        {
+            foreach (var m in messge.Where(m => size != m.Count))
+            {
+                for (var i = size - m.Count; i < size; i++)
+                {
+                    m.Insert(i, 0);
+                }
+            }
+            return messge;
+        }
+
+        public List<List<int>> DecryptMessage(List<int> message, int wi, int n, List<int> s)
+        {
+            var res = new List<List<int>>();
+            foreach (var num in message)
+            {
+                var temp = (num * wi) % 17;
+                var sumList = FindSum(s, temp);
+                res.Add(MapMessageList(s, sumList));
+            }
+
+            return res;
+        }
+
+        private List<List<int>> Split(List<char> source, int sub)
         {
             return source
                 .Select((x, i) => new { Index = i, Value = x })
@@ -34,5 +95,48 @@ namespace MerkleHellmanSimulation
                 .ToList();
         }
 
+        private List<int> MapMessageList(List<int> simple, List<int> sumList)
+        {
+            var res = new List<int>();
+            foreach (var num in simple)
+            {
+                //var temp = sumList.Any(s => s == num);
+                res.Add(sumList.Any(s => s == num) ? 1 : 0);
+            }
+            return res;
+        }
+        private List<int> FindSum(List<int> simple, int targetSum, int index = 0)
+        {
+
+            for (var i = index; i < simple.Count; i++)
+            {
+                int remainder = targetSum - simple[i];
+                // if the current number is too big for the target, skip
+                if (remainder < 0)
+                    continue;
+                // if the current number is a solution, return a list with it
+                if (remainder == 0)
+                    return new List<int>() { simple[i] };
+
+                // otherwise try to find a sum for the remainder later in the list
+                var s = FindSum(simple, remainder, i + 1);
+
+                // if no number was returned, we couldn't find a solution, so skip
+                if (s.Count == 0)
+                    continue;
+
+                // otherwise we found a solution, so add our current number to it
+                // and return the result
+                s.Insert(0, simple[i]);
+                return s;
+            }
+
+            // if we end up here, we checked all the numbers in the list and
+            // found no solution
+            return new List<int>();
+        }
+
     }
+
+
 }
